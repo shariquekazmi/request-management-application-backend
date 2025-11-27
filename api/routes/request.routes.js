afe5af3b0c5a24a524bf9b48faae7b21e4217e3d
shariquekazmi <iamshariquekazmi@gmail.com>
2025-11-27 09:18:30 +0530

    Request api's for CRUD operations

--- api/routes/request.routes.js
+++ api/routes/request.routes.js
@@ -5,6 +5,16 @@ import { authenticate } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 router.post("/create", authenticate, RequestController.createRequest);
-// later: approve, reject, action, close
+
+// Dynamic route for updating a request based on users role
+router.put("/:id/:action", authenticate, (req, res) =>
+  RequestController.updateRequestStatus(req, res)
+);
+
+// Fetch all requests based on the role based filter
+router.get("/", authenticate, RequestController.getAllRequests);
+
+// Get single request
+router.get("/:id", authenticate, RequestController.getRequestById);
 
 export default router;
